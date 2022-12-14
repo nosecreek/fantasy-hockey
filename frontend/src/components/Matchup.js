c5f35e8de0b7e2055d1da6c98650d9683b86dd94
Dustin Lammiman <dustin@nosecreekweb.ca>
2022-12-14 13:05:31 -0700

    intensify colors for Save %

--- frontend/src/components/Matchup.js
+++ frontend/src/components/Matchup.js
@@ -8,19 +8,26 @@ const Matchup = ({ teamStats, oppStats, league, week, setWeek, matchup }) => {
   const range = c1.range(c2, { space: 'hsl' })
 
   const valueToColor = (stat, other, id) => {
-    let x = (parseFloat(stat) / (parseFloat(stat) + parseFloat(other))) * 0.915
+    let x = parseFloat(stat) / (parseFloat(stat) + parseFloat(other))
 
     //custom calculation for plus/minus
     if (id === 4) {
       let offset = Math.min(parseInt(stat), parseInt(other))
       offset = offset < 0 ? Math.abs(offset) + 20 : 20
       x =
-        ((parseFloat(stat) + offset) /
-          (parseFloat(stat) + parseFloat(other) + offset * 2)) *
-        0.915
+        (parseFloat(stat) + offset) /
+        (parseFloat(stat) + parseFloat(other) + offset * 2)
+    }
+
+    //intensify colors for Save %
+    if (id === 26) {
+      x = x < 0.5 ? x * 0.95 : x * 1.05
     }
 
     x = id === 23 ? 1 - x : x //switch colors for GAA
+
+    x *= 0.915 //offset to set equal stats to yellow
+
     return range(x)
   }
 
