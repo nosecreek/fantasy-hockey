02d038b5de7b21bdd3ef28c48db8fe9385eac82a
Dustin Lammiman <dustin@nosecreekweb.ca>
2023-02-15 11:14:15 -0700

    Add github to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,8 @@
 const Footer = ({ setHelpScreen }) => {
   return (
     <div className="footer">
-      <a href="/auth/logout">Logout</a>
+      <a href="/auth/logout">Logout</a> |{' '}
+      <a href="https://github.com/nosecreek/fantasy-hockey">GitHub</a>
     </div>
   )
 }
